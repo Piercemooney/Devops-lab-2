b8d2f731ffb205f3b20413696e52d5d6d80629df
C22356391 Pierce Mooney <C22356391@mytudublin.ie>
2024-09-27 12:59:20 +0100

    Added

---
+++ avg.py
@@ -0,0 +1,9 @@
+# List of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Calculate the average
+if len(numbers) > 0:
+    average = sum(numbers) / len(numbers)
+    print("The average is:", average)
+else:
+    print("The list is empty, so the average cannot be calculated.")
